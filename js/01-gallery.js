65e74b3b28a86477677ae7a5102b6e42b3758bd9
GrzegorzKwiatkowski <117453707+GrzegorzKwiatkowski@users.noreply.github.com>
2023-05-05 18:53:53 +0200

    whatsGoingOn

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//const
+const galleryEl = document.querySelector('.gallery')
+
+//const
+//dodawanie zdjęć do galerii
+const galleryDiv = galleryItems.map(picture => `<a class=gallery__item href="${picture.original}"><img class=gallery__image src=${picture.preview} alt='${picture.description}'</img></a>`)
+.join('')
+galleryEl.insertAdjacentHTML('beforeend', galleryDiv)
+galleryEl.classList.add()
+//dodawanie zdjęć do galerii
+//lisener do powiekszania zdjęć
+galleryEl.addEventListener(click, openPicture => {
+    openPicture.preventDefault();
+    if (openPicture.target.nodeName !== 'IMG') {
+		return
+	}
+});
+
+
+
 
 console.log(galleryItems);
+console.log(galleryEl);
+console.log(galleryDiv)
+
