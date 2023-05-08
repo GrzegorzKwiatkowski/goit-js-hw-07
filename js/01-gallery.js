803ad948a27171875bf1fe5876173fe35ffe0eb1
GrzegorzKwiatkowski <Beo.kwiatkowski@gmail.com>
2023-05-09 00:25:10 +0200

    Comeback

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,70 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 //const
-const galleryEl = document.querySelector('.gallery')
+const galleryEl = document.querySelector(".gallery");
 
 //const
 //dodawanie zdjęć do galerii
-const galleryDiv = galleryItems.map(picture => `<a class=gallery__item href="${picture.original}"><img class=gallery__image src=${picture.preview} alt='${picture.description}'</img></a>`)
-.join('')
-galleryEl.insertAdjacentHTML('beforeend', galleryDiv)
-galleryEl.classList.add()
+function createGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+    })
+    .join("");
+}
+const cardsMarkup = createGallery(galleryItems);
+galleryEl.innerHTML = cardsMarkup;
 //dodawanie zdjęć do galerii
-//lisener do powiekszania zdjęć
-galleryEl.addEventListener(click, openPicture => {
-    openPicture.preventDefault();
-    if (openPicture.target.nodeName !== 'IMG') {
-		return
-	}
-});
+//EventListener + Modal
+galleryEl.addEventListener("click", openPicture);
 
+function openPicture(event) {
+  event.preventDefault();
 
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const openModal = () => {
+    const originalImg = event.target.dataset.source;
 
+    const instance = basicLightbox.create(
+      `
+      <img src="${originalImg}">
+      `,
+      {
+        onShow: (instance) => {
+          window.addEventListener("keydown", openModalbyEsc);
+        },
 
+        onClose: (instance) => {
+          window.removeEventListener("keydown", openModalbyEsc);
+        },
+      }
+    );
+
+    const openModalbyEsc = (event) => {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    };
+
+    instance.show();
+  };
+
+  openModal();
+}
+//EventListener + Modal
 console.log(galleryItems);
 console.log(galleryEl);
-console.log(galleryDiv)
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function createGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  `;
+    })
+    .join("");
+}
+const cardsMarkup = createGallery(galleryItems);
+galleryEl.innerHTML = cardsMarkup;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
